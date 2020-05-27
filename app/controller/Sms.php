255bb33056e42a8f2fa1f7e627fbef65cbb2624b
zhumingliang <353575156@qq.com>
2020-05-28 00:24:04 +0800

    测试

--- app/controller/Sms.php
+++ app/controller/Sms.php
@@ -29,7 +29,7 @@ class Sms extends BaseController
         $type = input('param.type', '', 'trim');
         $params = input('param.params', '', 'trim');
         // return $phoneNumber;
-        if (SmsBus::sendTemplate($phoneNumber, $type, $params, $sign)) {
+        if (SmsBus::sendTemplate($phoneNumber, $type, $params,'ali', $sign)) {
             return json(new SuccessMessageWithData(['msg' => '发送验证码成功']));
         }
         return json(new SaveException(['msg' => '发送验证码失败']));
