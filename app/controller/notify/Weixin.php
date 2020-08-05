82baab2e7befaaae461b9f6e86b1d9929437d854
zhumingliang <353575156@qq.com>
2020-08-06 00:36:55 +0800

    测试微信扫码支付

--- app/controller/notify/Weixin.php
+++ app/controller/notify/Weixin.php
@@ -19,11 +19,8 @@ class Weixin extends BaseController
 
     public function index()
     {
-        LogT::saveInfo('111');
         $app = (new \app\lib\pay\Weixin())->getApp();
         $response = $app->handlePaidNotify(function ($message, $fail) {
-            LogT::saveInfo(json_encode($message));
-
             // 使用通知里的 "微信支付订单号" 或者 "商户订单号" 去自己的数据库找到订单
             $order = SmsRechargeT::rechargeWithOrderNumber($message['out_trade_no']);
 
